02725252d8e8aa93421cd29a8ec88ad9e9432fb9
Andrey Gilevych <95359730+usernet2021@users.noreply.github.com>
2022-02-21 14:37:50 +0200

    Add files via upload

---
+++ Englishbook/blacktheme/blacktheme_style.php
@@ -0,0 +1,38 @@
+<?php 
+
+$products = [
+
+	[
+		'name'  => "English Grammar",
+		'img'   => "https://cv02.twirpx.net/2744/2744511.jpg?t=20191119031833",
+		'file'   => "http://xn--webducation-dbb.com/wp-content/uploads/2019/03/Raymond-Murphy-English-Grammar-in-Use-Cambridge-University-Press-2019.pdf",
+	],
+	[
+		'name'  => "Essential Grammar",
+		'img'   => "https://www.logobook.ru/make_nimage.php?uid=12772678",
+		'file'  => "http://www.freeonlinemusicacademy.com/English_PDF_Files/PDFs_files/Essential_Grammar_in_Use_-_3rd_Edition.pdf%E2%80%9D.pdf",
+	],
+	[
+		'name'  => "Advanced Grammar",
+		'img'   => "https://cdn1.ozone.ru/multimedia/1023228187.jpg",
+		'file'  => "http://144.122.235.170/ckfinder/userfiles/files/Advanced%20Grammar%20in%20Use%202nd%20Edition.pdf",
+	],
+	[
+		'name'  => "Oxford English",
+		'img'   => "https://go.alleng.org/d_images/engl_en/310_small.JPG",
+		'file'  => "https://drive.google.com/file/d/1T_iLqouT7wR9pG_DSpwCIfagXfdtGpkS/view",
+	],
+	[
+		'name'  => "Advanced Grammar",
+		'img'   => "https://go.alleng.org/d_images/engl_en/311_small.jpg",
+		'file'  => "https://drive.google.com/file/d/1tKWRDHH4mCGVyT6O6qHotFMvtMEA5M7b/view",
+	],
+	[
+		'name'  => "English Vocabulary",
+		'img'   => "https://dictionary.cambridge.org/ru/external/images/Vocabulary%20in%20Use%2004%20Advanced.jpg?version=5.0.213",
+		'file'  => "https://languageadvisor.net/wp-content/uploads/2021/06/English_Vocabulary_in_Use_Advanced_by_Michael_McCarthy_Felicity_ODell.pdf",
+	],
+	
+];
+
+?>
---
+++ Englishbook/index.php
@@ -0,0 +1,73 @@
+<?php 
+
+require_once "shop.php";
+
+// var_dump($products);
+if(!empty($_GET['file']))
+{
+	$filename = basename($_GET['file']);
+	$filepath = 'destination/' . $filename;
+	if(!empty($filename) && file_exists($filepath)){
+
+		header("Cache-Control: public");
+		header("Content-Description: FIle Transfer");
+		header("Content-Disposition: attachment; filename=$filename");
+		header("Content-Type: application/zip");
+		header("Content-Transfer-Emcoding: binary");
+
+		readfile($filepath);
+		exit;
+
+	}
+	else{
+		echo "This File Does not exist.";
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>EnglishBook</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/media.css">
+</head>
+<body>
+	<div class="decor1"></div>
+	<div class="decor1"></div>
+	<div id="block">
+		<nav>
+			<ul>
+				<li>
+					<a href="#" class="Logo">EnglishBook</a>
+				</li>
+				<li>
+					<a href="blacktheme/blacktheme.php" class="theme-black">
+						<ion-icon name="invert-mode-outline" class="theme"></ion-icon>
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<h1>Top English Book For Study</h1>
+		<main>
+			<div class="list">
+				<?php foreach ($products as $product) : ?>
+				<div class="book">
+					<img src="<?php echo $product['img']?>" alt="">
+					<h3><?php echo $product['name'] ; ?></h3>
+					<a href="<?php echo $product['file'] ?>" class="download">Download</a>
+				</div>
+				<?php endforeach; ?>
+			</div>
+		</main>
+		<footer>
+			<p>© EnglishBook</p>
+		</footer>
+	</div>
+<!-- SCRIPT -->
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
